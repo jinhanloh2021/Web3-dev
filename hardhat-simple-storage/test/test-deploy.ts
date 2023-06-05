c176decab6130f17c6a986ea76b16cfb63d52cb2
root <root@JINHANLAPTOP.localdomain>
2023-06-05 21:21:07 +0800

    Add test for add person function. Achieve 100% code coverage for tests.

--- hardhat-simple-storage/test/test-deploy.ts
+++ hardhat-simple-storage/test/test-deploy.ts
@@ -29,4 +29,32 @@ describe('SimpleStorage', () => {
     const currentValue = await simpleStorage.retrieve();
     assert.equal(currentValue.toString(), expectedValue);
   });
+
+  it('Person added to people array without mutation', async () => {
+    const transactionRes = await simpleStorage.addPerson(
+      'Jin Han',
+      BigNumber.from(42)
+    );
+    await transactionRes.wait(1);
+
+    const expectedPerson: [BigNumber, string] & {
+      favoriteNumber: BigNumber;
+      name: string;
+    } = [BigNumber.from(42), 'Jin Han'] as [BigNumber, string] & {
+      favoriteNumber: BigNumber;
+      name: string;
+    };
+    expectedPerson.favoriteNumber = BigNumber.from(42);
+    expectedPerson.name = 'Jin Han';
+
+    const addedPerson = await simpleStorage.people(BigNumber.from(0));
+
+    assert.equal(addedPerson[0].toString(), expectedPerson[0].toString());
+    assert.equal(addedPerson[1], expectedPerson[1]);
+    assert.equal(
+      addedPerson.favoriteNumber.toString(),
+      expectedPerson.favoriteNumber.toString()
+    );
+    assert.equal(addedPerson.name, expectedPerson.name);
+  });
 });
