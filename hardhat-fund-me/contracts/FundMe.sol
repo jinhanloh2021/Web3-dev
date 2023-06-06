c21ba4cf49e1ba4e8f9b6e7fcd703dbbc9ff1476
root <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-07 00:30:19 +0800

    Add documentation for FundMe.sol

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -1,27 +1,64 @@
 // SPDX-License-Identifier: MIT
+// Pragma
 pragma solidity ^0.8.0;
 
+// Imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
+// Error codes
+error FundMe__NotOwner();
+
+/**
+ * @title A contract for crows funding
+ * @author Jin Han
+ * @notice This contract is to demo a sample funding contract
+ * @dev This implements price feeds as our library
+ */
 contract FundMe {
+  // Type declarations
   using PriceConverter for uint256;
 
+  // State variables
   mapping(address => uint256) public addressToAmountFunded;
   address[] public s_funders;
   address public owner;
   AggregatorV3Interface public priceFeed;
+
+  modifier onlyOwner() {
+    // require(msg.sender == owner);
+    if(msg.sender != owner) revert FundMe__NotOwner();
+    _;
+  }
+  /**
+   * Functions order:
+   * constructor
+   * receive
+   * fallback
+   * external
+   * public
+   * internal
+   * private
+   * view/pure
+   */
 
   constructor(address _priceFeed) {
     priceFeed = AggregatorV3Interface(_priceFeed);
     owner = msg.sender;
   }
 
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
+  receive() external payable{
+    fund();
   }
 
+  fallback() external payable{
+    fund();
+  }
+
+  /**
+   * @notice This function funds this contract
+   * @dev This implements price feeds as our library
+   */
   function fund() public payable {
     uint256 minimumUSD = 50 * 10**18;
     require(
