e87ce72a80df44f285b58d445a47ad57c49f2fd2
root <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-07 02:25:41 +0800

    Finish tests for withdraw function. Need to account for gas when asserting. Use providers.

--- hardhat-fund-me/test/unit/FundMe.test.ts
+++ hardhat-fund-me/test/unit/FundMe.test.ts
@@ -1,7 +1,8 @@
 import { deployments, ethers, getNamedAccounts, network } from 'hardhat';
 import { FundMe, MockV3Aggregator } from '../../typechain-types';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { developmentChains } from '../../helper-hardhat-config';
+import { BigNumber } from 'ethers';
 
 describe('FundMe', () => {
   let fundMe: FundMe; // somehow typechain doesn't work here
@@ -18,11 +19,57 @@ describe('FundMe', () => {
     fundMe = await ethers.getContract('FundMe');
     mockV3Aggregator = await ethers.getContract('MockV3Aggregator');
   });
+
   describe('constructor', () => {
     it('Sets aggregator addresses correctly', async () => {
       const res_priceFeed_Address = await fundMe.priceFeed();
       const expected_priceFeed_Address = mockV3Aggregator.address;
       assert.equal(res_priceFeed_Address, expected_priceFeed_Address);
+    });
+  });
+
+  describe('fund', async () => {
+    it('Fails if not enough ETH sent', async () => {
+      await expect(fundMe.fund()).to.be.revertedWith(
+        'You need to spend more ETH!'
+      ); // expects fund() to revert if not enough eth
+    });
+    it('Updates the amount funded data structure', async () => {
+      await fundMe.fund({ value: BigNumber.from((1e18).toString()) });
+      const res = await fundMe.addressToAmountFunded(deployer);
+      assert.equal(
+        res.toString(),
+        BigNumber.from((1e18).toString()).toString()
+      );
+    });
+    it('Adds funders to array of funders', async () => {
+      await fundMe.fund({ value: BigNumber.from((1e18).toString()) });
+      const resFunder = await fundMe.s_funders(0);
+      assert.equal(resFunder, deployer);
+    });
+  });
+
+  describe('withdraw', async () => {
+    beforeEach(async () => {
+      await fundMe.fund({ value: BigNumber.from((1e18).toString()) }); // fund contract first
+    });
+    it('Withdraw ETH from a single founder', async () => {
+      // Arrange
+      const startFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const startDeployerBalance = await fundMe.provider.getBalance(deployer);
+      // Act
+      const transactionRes = await fundMe.withdraw();
+      const { gasUsed, effectiveGasPrice } = await transactionRes.wait(1); // waiting gets trans receipt
+      const endFundMeBalance = await fundMe.provider.getBalance(fundMe.address);
+      const endDeployerBalance = await fundMe.provider.getBalance(deployer);
+      // Assert
+      assert.equal(endFundMeBalance.toString(), BigNumber.from(0).toString());
+      assert.equal(
+        startFundMeBalance.add(startDeployerBalance).toString(),
+        endDeployerBalance.add(gasUsed.mul(effectiveGasPrice)).toString()
+      );
     });
   });
 });
