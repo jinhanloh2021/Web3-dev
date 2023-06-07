693ad8c182a274891cbbd8fb2c2f5fa26e75d88f
root <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-07 16:17:12 +0800

    Add withdraw test for multiple funders. Fix await bug. Abstract sendValue.

--- hardhat-fund-me/test/unit/FundMe.test.ts
+++ hardhat-fund-me/test/unit/FundMe.test.ts
@@ -3,11 +3,13 @@ import { FundMe, MockV3Aggregator } from '../../typechain-types';
 import { assert, expect } from 'chai';
 import { developmentChains } from '../../helper-hardhat-config';
 import { BigNumber } from 'ethers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
 describe('FundMe', () => {
   let fundMe: FundMe; // somehow typechain doesn't work here
   let deployer: string;
   let mockV3Aggregator: MockV3Aggregator;
+  const sendValue = BigNumber.from((1e18).toString());
 
   beforeEach(async () => {
     // deploy our FundMe contract using Hardhat-deploy
@@ -35,7 +37,7 @@ describe('FundMe', () => {
       ); // expects fund() to revert if not enough eth
     });
     it('Updates the amount funded data structure', async () => {
-      await fundMe.fund({ value: BigNumber.from((1e18).toString()) });
+      await fundMe.fund({ value: sendValue });
       const res = await fundMe.addressToAmountFunded(deployer);
       assert.equal(
         res.toString(),
@@ -43,7 +45,7 @@ describe('FundMe', () => {
       );
     });
     it('Adds funders to array of funders', async () => {
-      await fundMe.fund({ value: BigNumber.from((1e18).toString()) });
+      await fundMe.fund({ value: sendValue });
       const resFunder = await fundMe.s_funders(0);
       assert.equal(resFunder, deployer);
     });
@@ -51,9 +53,10 @@ describe('FundMe', () => {
 
   describe('withdraw', async () => {
     beforeEach(async () => {
-      await fundMe.fund({ value: BigNumber.from((1e18).toString()) }); // fund contract first
+      await fundMe.fund({ value: sendValue }); // fund contract first
     });
-    it('Withdraw ETH from a single founder', async () => {
+
+    it('Withdraw ETH from a single funder', async () => {
       // Arrange
       const startFundMeBalance = await fundMe.provider.getBalance(
         fundMe.address
@@ -70,6 +73,44 @@ describe('FundMe', () => {
         startFundMeBalance.add(startDeployerBalance).toString(),
         endDeployerBalance.add(gasUsed.mul(effectiveGasPrice)).toString()
       );
+    });
+
+    it('Allows us to withdraw with multiple funders', async () => {
+      // Arrange
+      const accounts: SignerWithAddress[] = await ethers.getSigners();
+      for (let i = 1; i < 6; i++) {
+        // fundMe connected to deploy, need to reconnect with other accounts
+        await fundMe.connect(accounts[i]).fund({
+          value: sendValue,
+        });
+      }
+      const startFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const startDeployerBalance = await fundMe.provider.getBalance(deployer);
+      // Act
+      const transactionRes = await fundMe.withdraw();
+      const transactionReceipt = await transactionRes.wait(1);
+      const { gasUsed, effectiveGasPrice } = transactionReceipt; // waiting gets trans receipt
+
+      // Assert
+      const endFundMeBalance = await fundMe.provider.getBalance(fundMe.address);
+      const endDeployerBalance = await fundMe.provider.getBalance(deployer);
+      // console.log(
+      //   `Start FundMe: ${startFundMeBalance}\nStart Deploy: ${startDeployerBalance}\nEnd FundMe: ${endFundMeBalance}\nEnd Deploy: ${endDeployerBalance}\n`
+      // );
+      assert.equal(endFundMeBalance.toString(), BigNumber.from(0).toString());
+      assert.equal(
+        startFundMeBalance.add(startDeployerBalance).toString(),
+        endDeployerBalance.add(gasUsed.mul(effectiveGasPrice)).toString()
+      );
+      await expect(fundMe.s_funders(BigNumber.from(0))).to.be.reverted;
+      for (let i = 1; i < 6; i++) {
+        assert.equal(
+          (await fundMe.addressToAmountFunded(accounts[i].address)).toString(),
+          BigNumber.from(0).toString()
+        );
+      }
     });
   });
 });
