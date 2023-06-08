f0d0c98f9304eccc0ccbf5654972e7a88054af62
root <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-08 16:51:40 +0800

    Extend Raffle from VRFConsumerBaseV2 to implement VRF and fulfil random words. Need randomness to select winner of lottery.

--- hardhat-lottery/contracts/Raffle.sol
+++ hardhat-lottery/contracts/Raffle.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 // Enter the lottery (paying some amount)
 // Pick a random winner (verifiably random)
 // Winner to be selected every X minutes -> completely automated
 // Chainlink Oracle -> VRF, Automated Execution with Chainlink Keepers
 
-
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2{
     /** State variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -17,7 +17,7 @@ contract Raffle {
     /** Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 _entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 _entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
         i_entranceFee = _entranceFee;
     }
 
@@ -30,7 +30,12 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external{
+        // Request random number - 2 step process
+        // Do something with number
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
 
     function getEntranceFee() public view returns(uint256) {
         return i_entranceFee;
