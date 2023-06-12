070f57d9f9d7a124617f20fe3bc0044fa83a4957
root <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-12 16:14:14 +0800

    Add lending pool address.

--- hardhat-defi/hardhat.config.ts
+++ hardhat-defi/hardhat.config.ts
@@ -8,6 +8,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       { version: '0.8.7' },
+      { version: '0.6.12' },
       { version: '0.6.6' },
       { version: '0.4.19' },
     ],
--- hardhat-defi/scripts/aaveBorrow.ts
+++ hardhat-defi/scripts/aaveBorrow.ts
@@ -1,6 +1,37 @@
+import { ethers, getNamedAccounts } from 'hardhat';
+import getWeth from '../scripts/getWeth';
+import {
+  ILendingPool,
+  ILendingPoolAddressesProvider,
+} from '../typechain-types';
 const main = async () => {
-    //protocol treats everything as ERC20. Need to wrap ETH to Wrapped ETH WETH.
-}
+  //protocol treats everything as ERC20. Need to wrap ETH to Wrapped ETH WETH.
+  await getWeth();
+  const { deployer } = await getNamedAccounts();
+  // abi, address
+  // Lending pool address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+  // Lending pool
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`LendingPool addres: ${lendingPool}`);
+
+  // Deposit
+};
+
+const getLendingPool = async (account: string): Promise<ILendingPool> => {
+  const lendingPoolAddressProvider: ILendingPoolAddressesProvider =
+    await ethers.getContractAt(
+      'ILendingPoolAddressesProvider',
+      '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+      account
+    );
+  const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool();
+  const lendingPool: ILendingPool = await ethers.getContractAt(
+    'ILendingPool',
+    lendingPoolAddress,
+    account
+  );
+  return lendingPool;
+};
 
 main()
   .then(() => process.exit(0))
