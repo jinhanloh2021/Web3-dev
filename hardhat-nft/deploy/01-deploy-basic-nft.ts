20977cd68049340fca5a781d82bee2568bcf8f1d
jinhan <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-13 16:08:13 +0800

    Add basic nft and deployment. Use ERC721 standard from openzeppelin.

---
+++ hardhat-nft/deploy/01-deploy-basic-nft.ts
@@ -0,0 +1,42 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import 'dotenv/config';
+import { network } from 'hardhat';
+import verify from '../utils/verify';
+import {
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+  developmentChains,
+} from '../helper-hardhat-config';
+
+const deployBasicNft = async ({
+  getNamedAccounts,
+  deployments,
+}: HardhatRuntimeEnvironment) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+  log('-'.repeat(54));
+  const basicNft = await deploy('BasicNft', {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log(`Verifying contract ${basicNft.address} on ${network.name}...`);
+    await verify(basicNft.address, []);
+    log(`Contract verified`);
+    log('-'.repeat(54));
+  } else {
+    log(`${network.name} network detected. Verification not needed.`);
+    log('-'.repeat(54));
+  }
+};
+
+export default deployBasicNft;
